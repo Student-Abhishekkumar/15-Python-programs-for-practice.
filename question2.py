06c09834861df2cb9e6c2f08c7cb5e523518cca5
Abhishek Kumar <158078358+Student-Abhishekkumar@users.noreply.github.com>
2024-02-20 00:03:26 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,3 +1,8 @@
+'''Write a program that takes a student's score as input and prints out their corresponding grade. 
+If the score is above or equal to 90, the grade is an 'A'. If it's between 80 and 89, the grade is 'B'. 
+If it's between 70 and 79, the grade is 'C'. If it's between 60 and 69, the grade is 'D'. 
+Otherwise, the grade is 'F'.'''
+
 score=float(input("Enter your score : "))
 if score in range(90,101):
     print("Grade A")
