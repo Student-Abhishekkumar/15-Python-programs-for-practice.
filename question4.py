bdabfc2b353d59248bf29aa00358cc1776dc9a28
Abhishek Kumar <158078358+Student-Abhishekkumar@users.noreply.github.com>
2024-02-20 00:04:58 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,3 +1,6 @@
+'''Write a program that categorizes a person's age into different groups: "Child" (0-12 years), "Teenager" (13-19 years), 
+"Adult" (20-59 years), and "Senior" (60 years and above). Ask the user to input their age and print the corresponding category.'''
+
 age=int(input("Enter your age : "))
 if age in range(0,13):
     print("Child")
