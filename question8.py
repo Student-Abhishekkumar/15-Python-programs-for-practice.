4ed8b32a7ba0871bf766e296927680dde1171c8b
Abhishek Kumar <158078358+Student-Abhishekkumar@users.noreply.github.com>
2024-02-20 00:09:46 +0530

    Update question8.py

--- question8.py
+++ question8.py
@@ -1,3 +1,7 @@
+'''Harry & Meghan were playing a game. Harry gave Meghan a number N, and asked her to multiply it by 3, 
+and then add 7 to it, and then finally subtract 10 from it. Since Meghan is very weak in Math, 
+help her win the game by finding the final value of N.'''
+
 n=int(input("Enter your number : "))
 num=((n*3)+7)-10
 print("Answer is : ",num)
