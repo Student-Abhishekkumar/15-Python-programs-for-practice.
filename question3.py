9c414f52fd76529c10b988da5770eb9d8c22afb6
Abhishek Kumar <158078358+Student-Abhishekkumar@users.noreply.github.com>
2024-02-20 00:04:23 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,3 +1,7 @@
+'''Write a program to check if a given year is a leap year or not. 
+A year is a leap year if it is divisible by 4 but not divisible by 100 unless it is also divisible by 400. 
+Display an appropriate message based on whether the given year is a leap year or not.'''
+
 y=int(input("Enter year : "))
 if(y%4==0 and y%100!=0 or y%400==0):
     print("Year is a leap year.")
