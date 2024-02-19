5ef454f85ca8404a3d5e85fd6d829889c7eb3ab7
Abhishek Kumar <158078358+Student-Abhishekkumar@users.noreply.github.com>
2024-02-20 00:05:34 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,3 +1,8 @@
+'''You're writing a program for a cinema that calculates the ticket price based on the age of the customer.
+For customers aged 12 or below, the ticket price is $5. For customers aged between 13 and 59, the ticket price is $10. 
+For customers aged 60 and above, the ticket price is $7. 
+Write the code to calculate the ticket price based on the age entered by the user.'''
+
 age=int(input("Enter your age : "))
 if(age<=12):
     print("Price : $5")
